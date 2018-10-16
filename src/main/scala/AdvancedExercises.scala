77fdad2231b1e70e02de3a6100e832f4b288d82d
davidrymer <gotrees2012>
2018-10-16 17:08:47 +0100

    More anagram stuff

--- src/main/scala/AdvancedExercises.scala
+++ src/main/scala/AdvancedExercises.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
@@ -9,15 +10,17 @@ object AdvancedExercises {
     def accumulateWords(): ArrayBuffer[String] = {
       var dictionary = ArrayBuffer[String]()
       val bufferedSource = Source.fromFile("src/enable1.txt")
+
       for (line <- bufferedSource.getLines) {
         dictionary += line.toString
-
       }
       dictionary
     }
 
-    def generateWords(words: String*):Unit = {
+    def generateWords(words: String*):mutable.HashMap[String, ArrayBuffer[String]] = {
       var anagrams = ArrayBuffer[String]()
+      val hashMap = new mutable.HashMap[String, ArrayBuffer[String]]
+
       words.foreach{word =>
         accumulateWords().foreach{ element =>
           if (element.toCharArray.sorted.sameElements(word.toLowerCase.toCharArray.sorted))
@@ -25,16 +28,32 @@ object AdvancedExercises {
         }
 
         anagrams.nonEmpty match {
-          case true => anagrams.foreach(println)
-
+          case true =>
+          //  anagrams.foreach(println)
+            // println()
+            hashMap += word -> anagrams
           case _ => println(s"No word found for \'$word\'")
         }
 
         anagrams.clear()
       }
+      println(hashMap)
+
+
+      hashMap
     }
-    generateWords("pol", "hello", "lol")
+    generateWords("ppa", "cats")
+
+    def mostAnagrams(anagrams: mutable.HashMap[String, ArrayBuffer[String]]): Unit = {
+      println(anagrams.get("ppa").get.length)
+    }
+
+
+
+//    mostAnagrams(generateWords("cats", "ppa"))
   }
+
+
 
 
 }
